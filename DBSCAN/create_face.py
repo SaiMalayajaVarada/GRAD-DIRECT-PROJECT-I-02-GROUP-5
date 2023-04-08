e8b02c78983179f401c24b38991565836dba7040
zabheenshaik <98142661+zabheenshaik@users.noreply.github.com>
2023-04-07 22:13:41 -0500

    #importing the lib

--- DBSCAN/create_face.py
+++ DBSCAN/create_face.py
@@ -7,6 +7,5 @@ root_path = 'face_images/'
 dir_name, data_len = enc.create_face_database(root_path)
 unq_faces, res_dir = cluster.do_cluster(dir_name)
 # Apply face clustering to retrieve unique faces
-
 # print("Number of unique face detected: ", unq_faces)
 # print(f'You can find clustered face in "{res_dir}" directory')

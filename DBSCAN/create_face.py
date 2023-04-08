dda24284075e97993a1eec2405f5596c1ae7f32e
zabheenshaik <98142661+zabheenshaik@users.noreply.github.com>
2023-04-07 22:10:01 -0500

    #adding code for unq faces

--- DBSCAN/create_face.py
+++ DBSCAN/create_face.py
@@ -5,8 +5,8 @@ root_path = 'face_images/'
 
 # Create a face database from existing faces
 dir_name, data_len = enc.create_face_database(root_path)
-
-# Apply face clustering to retrieve unique faces
 unq_faces, res_dir = cluster.do_cluster(dir_name)
+# Apply face clustering to retrieve unique faces
+
 # print("Number of unique face detected: ", unq_faces)
 # print(f'You can find clustered face in "{res_dir}" directory')
